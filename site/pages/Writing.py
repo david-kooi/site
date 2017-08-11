f99ecef3d3138139e80107a84812597527f1fa84
David Kooi <kooi.david.w@gmail.com>
2017-08-10 21:29:54 -0700

    Writing path work

--- site/pages/Writing.py
+++ site/pages/Writing.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import pprint
 import subprocess
 
 from flask import Blueprint, render_template, url_for
@@ -10,7 +11,10 @@ from pages.page import Page
 class Writing(Page):
     def __init__(self, *args, **kwargs):
         self.root = ''
-        self.link_cache= {} 
+        self.tree = {'/writing':
+                        {'items':{}
+                        }
+                    }
 
         self.base_info = {'image_path'  : 'images/writing.jpg',
                           'title'       : 'Writing',
@@ -33,9 +37,15 @@ Writing = Writing("Writing", __name__, template_folder='templates')
 @Writing.route('/writing')
 def WritingView():
 
-    link_dict, link_names = GetLinkDict('/writing')
-    Writing.link_dict = link_dict
 
+
+    SetTreeFromRoot()
+    #pprint.PrettyPrinter(indent=1).pprint(Writing.tree['/writing']['items'])
+    
+    link_dict, link_names= GetLinkDict("/writing")
+    #print(link_dict)
+    
+    Writing.base_info['content_type'] = 'links'
 
     return render_template('left_image.html', **Writing.base_info, link_names=link_names,\
                                               link_dict=link_dict) 
@@ -44,8 +54,11 @@ def WritingView():
 def ContentView(file_path):
     
     file_root = os.path.join('/writing', file_path) 
+    print(file_root)
     link_dict, link_names = GetLinkDict(file_root) # Get file name
+    #print(link_dict)
 
+    Writing.base_info['content_type'] = 'links'
 
     return render_template('left_image.html', **Writing.base_info, link_names=link_names,\
                                               link_dict=link_dict) 
@@ -58,58 +71,74 @@ def GetLatestCommitSha():
     cmd = "curl  \"https://api.github.com/repos/david-kooi/writing/commits\""
     output = subprocess.check_output(cmd, shell=True)
     output = output.decode("utf-8")
-    print("OUTPUT: {}".format(output))
+    #print("OUTPUT: {}".format(output))
 
     return json.loads(output)[0]['sha']
 
-def GetLinkDict(base_file_path):
- 
-    link_names = []
-    file_root = os.path.dirname(base_file_path)
+def SetTreeFromRoot():
 
-    # Continue down the file tree
- 
-    print("FILE PATH: {}".format(base_file_path))
-    Writing.root = file_root 
-
-    if(base_file_path == '/writing'):
-        sha = GetLatestCommitSha()
-    else:
-        print(Writing.link_cache)
-        sha = Writing.link_cache[base_file_path]['sha'] 
-
-
-    print("")
-    print("")
+    sha = GetLatestCommitSha()
 
     cmd = "curl  \"https://api.github.com/repos/david-kooi/writing/git/trees/"\
-                 "{}\"".format(sha)
+                 "{}\"?recursive=1".format(sha)
     output = subprocess.check_output(cmd, shell=True)
     output = output.decode('utf-8') 
 
 
-    git_tree = json.loads(output)['tree']
- 
+    tree_list = json.loads(output)['tree'] # Tree is a list of dictionaries
 
-    for f in git_tree:
-        file_name   = f['path']
-        url         = f['url']
-        content_t   = f['type']
-        sha         = f['sha']
-        
-        if(file_name[0] == '.'):
+    for item in tree_list: 
+        if(item['path'][0] == '.'):
             continue
+
+
+        root = os.path.join('/writing', item['path'])
+       
+        if(item['type'] == 'tree'):
+            Writing.tree[root] = {}
+            Writing.tree[root]['tree'] = item
+            Writing.tree[root]['items'] = {}
+
+        elif(item['type'] == 'blob'): 
+            file_root = os.path.dirname(root)
+            file_name = root.split('/')[-1]
+
+            Writing.tree[file_root]['items'][file_name] = item
+
+
+
+def GetLinkDict(base_path):
  
-        file_path = os.path.join(base_file_path, file_name)
-        Writing.link_cache[file_path] = {}
-        Writing.link_cache[file_path] =    {'url':url, 'content_t':content_t,\
-                                            'sha':sha, 'path':file_path, 'name':file_name} 
-        link_names.append(file_path)
-     
-    link_dict = Writing.link_cache
-    link_names = sorted(link_names)
-    Writing.link_cache[file_path]['link_names'] = link_names
+    link_names = []
+    link_dict  = {}
 
 
+    # Collect from Writing.tree 
+ 
+
+    base_path_tokens = base_path.split('/')
+    for item_path in Writing.tree:
+        item_path_tokens = item_path.split('/')
+        
+        if(len(item_path_tokens) == len(base_path_tokens) + 1):
+            item = Writing.tree[item_path]['tree'] 
+        
+
+            file_name   = os.path.basename(item['path'])
+            path        = os.path.join(base_path, file_name) 
+            content_t   = item['type']
+            sha         = item['sha']
+            
+            if(file_name[0] == '.'):
+                continue
+      
+            link_names.append(file_name)
+            link_dict[file_name] = {}
+            link_dict[file_name]['path']  = path 
+            link_dict[file_name]['type']  = content_t
+            link_dict[file_name]['sha']   = sha
+         
+        link_names = sorted(link_names)
+        
     return link_dict, link_names
  
