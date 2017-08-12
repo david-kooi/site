1819182a83c763256c005458fffe5c0eacc709c2
David Kooi <kooi.david.w@gmail.com>
2017-08-11 19:47:26 -0700

    Successfully pulling files from github

--- site/pages/Writing.py
+++ site/pages/Writing.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import base64
+import codecs
 import pprint
 import subprocess
 
@@ -12,7 +14,7 @@ class Writing(Page):
     def __init__(self, *args, **kwargs):
         self.root = ''
         self.tree = {'/writing':
-                        {'items':{}
+                        {'item_dict':{}
                         }
                     }
 
@@ -37,31 +39,59 @@ Writing = Writing("Writing", __name__, template_folder='templates')
 @Writing.route('/writing')
 def WritingView():
 
-
-
     SetTreeFromRoot()
-    #pprint.PrettyPrinter(indent=1).pprint(Writing.tree['/writing']['items'])
+    pprint.PrettyPrinter(indent=1).pprint(Writing.tree['/writing']['item_dict'])
     
     link_dict, link_names= GetLinkDict("/writing")
     #print(link_dict)
+   
+    # Get file items
+    item_dict = Writing.tree['/writing']['item_dict']
     
     Writing.base_info['content_type'] = 'links'
+    Writing.base_info['item_dict']    = item_dict
+    Writing.base_info['link_dict']    = link_dict
+    Writing.base_info['link_names']   = link_names
 
-    return render_template('left_image.html', **Writing.base_info, link_names=link_names,\
-                                              link_dict=link_dict) 
+    return render_template('left_image.html', **Writing.base_info) 
+
 
 @Writing.route('/writing/<path:file_path>')
 def ContentView(file_path):
     
-    file_root = os.path.join('/writing', file_path) 
-    print(file_root)
-    link_dict, link_names = GetLinkDict(file_root) # Get file name
-    #print(link_dict)
+    file_root   = os.path.join('/writing', file_path) 
+    folder_root = os.path.dirname(file_root) 
+    file_name   = os.path.basename(file_root)
 
-    Writing.base_info['content_type'] = 'links'
+    if('.' in file_name): # Treat as file 
 
-    return render_template('left_image.html', **Writing.base_info, link_names=link_names,\
-                                              link_dict=link_dict) 
+        # Get folder items to get file url
+        item_dict = Writing.tree[folder_root]['item_dict']
+        url = item_dict[file_name]['url']
+
+        info = GetBlobFromGithub(url) 
+
+        # Set page info
+        Writing.base_info['content_type']  = 'text'
+        Writing.base_info['info']          = info
+
+
+    else: # Treat as folder
+        
+        link_dict, link_names = GetLinkDict(file_root) # Get file name
+        #print(link_dict)
+
+        # Get file items
+        item_dict = Writing.tree[file_root]['item_dict']
+
+        # Set page info
+        Writing.base_info['content_type'] = 'links'
+        Writing.base_info['item_dict']    = item_dict
+        Writing.base_info['link_dict']    = link_dict
+        Writing.base_info['link_names']   = link_names
+
+
+    return render_template('left_image.html', **Writing.base_info)
 
 
 
@@ -74,6 +104,16 @@ def GetLatestCommitSha():
     #print("OUTPUT: {}".format(output))
 
     return json.loads(output)[0]['sha']
+
+def GetBlobFromGithub(url):
+    cmd = "curl {}".format(url)
+    output = subprocess.check_output(cmd, shell=True)
+    output = output.decode('utf-8')
+    content = json.loads(output)['content']
+    content = base64.b64decode(content)
+    content = content.decode('utf-8')
+    
+    return content 
 
 def SetTreeFromRoot():
 
@@ -88,22 +128,26 @@ def SetTreeFromRoot():
     tree_list = json.loads(output)['tree'] # Tree is a list of dictionaries
 
     for item in tree_list: 
-        if(item['path'][0] == '.'):
+        file_name = os.path.basename(item['path'])
+        if(file_name[0] == '.'):
             continue
 
-
+        # Add /writing to the path
         root = os.path.join('/writing', item['path'])
        
         if(item['type'] == 'tree'):
             Writing.tree[root] = {}
             Writing.tree[root]['tree'] = item
-            Writing.tree[root]['items'] = {}
+            Writing.tree[root]['item_dict'] = {}
 
         elif(item['type'] == 'blob'): 
             file_root = os.path.dirname(root)
             file_name = root.split('/')[-1]
 
-            Writing.tree[file_root]['items'][file_name] = item
+            # Add /writing to the path
+            item['path'] = root
+
+            Writing.tree[file_root]['item_dict'][file_name] = item
 
 
 
@@ -119,25 +163,30 @@ def GetLinkDict(base_path):
     base_path_tokens = base_path.split('/')
     for item_path in Writing.tree:
         item_path_tokens = item_path.split('/')
-        
-        if(len(item_path_tokens) == len(base_path_tokens) + 1):
-            item = Writing.tree[item_path]['tree'] 
-        
 
-            file_name   = os.path.basename(item['path'])
-            path        = os.path.join(base_path, file_name) 
-            content_t   = item['type']
-            sha         = item['sha']
-            
-            if(file_name[0] == '.'):
-                continue
-      
-            link_names.append(file_name)
-            link_dict[file_name] = {}
-            link_dict[file_name]['path']  = path 
-            link_dict[file_name]['type']  = content_t
-            link_dict[file_name]['sha']   = sha
-         
+        
+        # Get item if item_path is 1 deeper than base_path
+        if(len(item_path_tokens) == len(base_path_tokens) + 1):
+            # Check if item_path continues from base_path
+            if(item_path_tokens[-2] == base_path_tokens[-1]): 
+
+                
+                item = Writing.tree[item_path]['tree']  
+
+                file_name   = os.path.basename(item['path'])
+                path        = os.path.join(base_path, file_name) 
+                content_t   = item['type']
+                sha         = item['sha']
+                
+                if(file_name[0] == '.'):
+                    continue
+          
+                link_names.append(file_name)
+                link_dict[file_name] = {}
+                link_dict[file_name]['path']  = path 
+                link_dict[file_name]['type']  = content_t
+                link_dict[file_name]['sha']   = sha
+             
         link_names = sorted(link_names)
         
     return link_dict, link_names
